cd3eb29822ba3f4914c28f1be3c17a9eb29a48e7
winstonbarbe <winston.barbe@gmail.com>
2021-02-21 12:55:39 -0500

    creates a persons service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  persons = ["Me", "Myself", "Irene"]
-
-  onPersonCreated(name: string) {
-     this.persons.push(name)
-  }
-}
+export class AppComponent {}
---
+++ src/app/persons/persons.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({providedIn: "root"})
+export class PersonsService {
+  persons: string[] = ["Me", "Myself", "Irene"]
+
+  addPerson(name: string) {
+     this.persons.push(name)
+  }
+}
