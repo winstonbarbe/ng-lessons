5dee7326f24697f10fbaa695a7167029367e826a
winstonbarbe <winston.barbe@gmail.com>
2021-02-21 13:46:04 -0500

    implements lifecycle on init class

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,10 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersonsService } from './persons.service';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html'
 })
 
-export class PersonsComponent {
-  @Input() personsList: string[];
+export class PersonsComponent implements OnInit {
+  personsList: string[];
+  constructor(private prsService: PersonsService) {
+    // this.personsList= prsService.persons;
+  }
+  ngOnInit() {
+    this.personsList = this.prsService.persons;
+  }
 }
+ 
